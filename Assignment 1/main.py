c3348391126775d82c7e09a3f12c21b0a624bf52
TennantQuinton <quintonimo1998@gmail.com>
2021-09-20 09:56:04 -0400

    Update main.py

--- Assignment 1/main.py
+++ Assignment 1/main.py
@@ -1,4 +1,16 @@
+import os
 # Write your assignment here
 
-if __name__ == '__main__':
-    print('hello')
+my_absolute_dirpath = os.path.abspath(os.path.dirname(__file__))
+
+data_in_1 = open("{0}/data/1.in".format(my_absolute_dirpath), "r")
+data_in_2 = open("{0}/data/2.in".format(my_absolute_dirpath), "r")
+print('data_in_1')
+for line in data_in_1:
+    line = line.replace('\n', '')
+    print(line)
+print()
+print('data_in_2')
+for line in data_in_2:
+    line = line.replace('\n', '')
+    print(line)
