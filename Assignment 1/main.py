095a984e748a86aca221110da67e45391f679e07
TennantQuinton <quintonimo1998@gmail.com>
2021-09-22 19:08:47 -0400

    Update main.py
    I think I got GD working properly here. Will have to test some more

--- Assignment 1/main.py
+++ Assignment 1/main.py
@@ -2,13 +2,13 @@ import os, json, argparse, sys
 import numpy as np, matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 
-def linear_regression(input_data):
+def linearRegression(input_data):
     # Slicing the data into the first M-1 columns and the final column
     y_dat = np.array(input_data[:,-1]).T
     x_dat = np.array(input_data[:,0:-1])
 
     # Making the first column of the design matrix 1s
-    ones = [1] * (len(input_data))
+    ones = np.ones(len(input_data))
     x_dat = np.insert(x_dat, 0, ones, axis=1)
 
     # Going step-by-step through finding w
@@ -26,25 +26,49 @@ def linear_regression(input_data):
     # Output the results (Need to output to .out file)
     return multiply_3
 
-def grad_descent(input_data, learning_rate, num_iter):
-    print("hello world")
+def gradDescent(input_data, learning_rate, num_iter):
+    w_0 = np.zeros(len(input_data[0]))
 
+    # Slicing the data into the first M-1 columns and the final column
+    y_dat = np.array(input_data[:,-1]).T
+    x_dat = np.array(input_data[:,0:-1])
 
-n = "1"
+    # Making the first column of the design matrix ones
+    ones = np.ones(len(input_data))
+    x_dat = np.insert(x_dat, 0, ones, axis=1)
+
+    # transposing x outside of our loop for efficiency
+    transpose_x = x_dat.T
+
+    # Initializing iteration count for loop
+    iteration = 0
+    while iteration <= num_iter:
+        #print(iteration)
+        a = np.matmul(x_dat, w_0)
+        L = a - y_dat
+
+        gradient = np.matmul(transpose_x, L)/(len(input_data))
+
+        w_0 = w_0 - (learning_rate*gradient)
+        iteration+=1
+    return(w_0)
+
+n = 2
 
 # Getting the absolute file path from the relative
 my_absolute_dirpath = os.path.abspath(os.path.dirname(__file__))
 
 # Reading in the data and running the linear regression (Need to change to getting from command line input)
 data_in = np.loadtxt("{0}/data/{1}.in".format(my_absolute_dirpath, n))
-print(linear_regression(data_in))
 
 
 json_input_file = "{0}/data/{1}.json".format(my_absolute_dirpath, n)
+
 with open(json_input_file, 'r') as file:
     inputs = json.load(file)
 
     learning_rate = inputs['learning rate']
     num_iter = inputs['num iter']
 
-print(grad_descent(data_in, learning_rate, num_iter))
+print("w_analytic: {0}".format(linearRegression(data_in)))
+print("w_GD: {0}".format(gradDescent(data_in, learning_rate, num_iter)))
