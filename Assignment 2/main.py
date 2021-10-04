5acb60d63db7a82d38aae971412500925c722427
TennantQuinton <quintonimo1998@gmail.com>
2021-10-04 18:40:53 -0400

    Update main.py
    Had to change number of classes to be larger than the largest digit (8)

--- Assignment 2/main.py
+++ Assignment 2/main.py
@@ -9,54 +9,85 @@ import os, json, argparse, numpy as np, matplotlib.pyplot as plt, pandas as pd
 import torch as T, torch.nn as nn, torchvision as TV, torchvision.transforms as tf, torch.optim as optim
 from torchvision.utils import make_grid
 
-def digit_id(input_data):
-    # Training data split (take the first 26492 digits)
-    training = (input_data[0:-2999])
+def conversion(input_data):
+    # Training data split (take the middle 20992 digits) in Pandas df format
+    training = (input_data[5500:-2999])
     training_data = training[training.columns[:-1]]
     training_labels = training[training.columns[-1]]
 
-    # Testing data split (take the final 3000 digits)
+    # Set 5500 of the digits as a validation set in Pandas df format
+    validation = (input_data[0:5501])
+    validation_data = validation[validation.columns[:-1]]
+    validation_labels = validation[validation.columns[-1]]
+
+    # Testing data split (take the final 3000 digits) in Pandas df format
     testing = (input_data[26491:])
     testing_data = testing[testing.columns[:-1]]
     testing_labels = testing[testing.columns[-1]]   
 
-    # Convert to array
+    # Convert to numpy array
     training_data = np.array(training_data)
     training_labels = np.array(training_labels)
+    validation_data = np.array(validation_data)
+    validation_labels = np.array(validation_labels)
     testing_data = np.array(testing_data)
     testing_labels = np.array(testing_labels)
 
     # Reshape to the correct dimensions for analysis
     training_data = training_data.reshape((-1, 1, 14, 14))
     training_labels = training_labels.reshape(training_labels.shape[0])
+    validation_data = validation_data.reshape((-1, 1, 14, 14))
+    validation_labels = validation_labels.reshape(validation_labels.shape[0])
     testing_data = testing_data.reshape((-1, 1, 14, 14))
     testing_labels = testing_labels.reshape(testing_labels.shape[0])
 
-    # Convert to PyTorch tensor and normalize
+    # Convert to PyTorch tensor and normalize values
     training_data_tens = T.tensor(training_data)/255.0
     training_labels_tens = T.tensor(training_labels)
+    validation_data_tens = T.tensor(validation_data)/255.0
+    validation_labels_tens = T.tensor(validation_labels)
     testing_data_tens = T.tensor(testing_data)/255.0
     testing_labels_tens = T.tensor(testing_labels)
 
-    # Convert to tensors and load the data so that PyTorch can use it effectively
+    batch_size = 100
+
+    # Convert to tensor with both data and labels. Then load the data
     training_tensor = T.utils.data.TensorDataset(training_data_tens, training_labels_tens)
     testing_tensor = T.utils.data.TensorDataset(testing_data_tens, testing_labels_tens)
-    load_training = T.utils.data.DataLoader(training_tensor, batch_size = 64, shuffle = True)
-    load_testing = T.utils.data.DataLoader(testing_tensor, batch_size = 64, shuffle = True)
+    validation_tensor = T.utils.data.TensorDataset(validation_data_tens, validation_labels_tens)
+    load_training = T.utils.data.DataLoader(training_tensor, batch_size = batch_size, shuffle = True)
+    load_testing = T.utils.data.DataLoader(testing_tensor, batch_size = batch_size, shuffle = True)
+    load_validation = T.utils.data.DataLoader(validation_tensor, batch_size = batch_size, shuffle = True)
+
+    return (load_training, load_testing)
+
+def training(load_training, load_testing):
 
     dataiter = iter(load_training)
     images, labels = dataiter.next()
 
     input_size = 14*14 # =196
     hidden_layer = 100
-    output_size = 5
+    output_size = 9
+    learning_rate = 0.001
 
     model = nn.Sequential(nn.Linear(input_size, hidden_layer),
                             nn.ReLU(),
-                            nn.Linear(hidden_layer, hidden_layer),
-                            nn.ReLU(),
                             nn.Linear(hidden_layer, output_size),
                             nn.LogSoftmax(dim=1))
+
+    optimizer = optim.Adam(model.parameters(), learning_rate)
+    criterion = nn.NLLLoss()
+    images, labels = next(iter(load_training))
+    images = images.view(images.shape[0], -1)
+
+    log_probs = model(images)
+    loss = criterion(log_probs, labels)
+
+    print('Before backward pass: \n', model[0].weight.grad)
+    loss.backward()
+    print('After backward pass: \n', model[0].weight.grad)
+    
 
 if __name__ == '__main__':
     # Command line arguments
@@ -73,5 +104,6 @@ if __name__ == '__main__':
     my_absolute_dirpath = os.path.abspath(os.path.dirname(__file__))
 
     data_in = pd.read_csv("{0}/{1}".format(my_absolute_dirpath, in_file), delimiter=' ')
-    digit_id(data_in)
+    converted_data = conversion(data_in)
+    training(converted_data[0], converted_data[1])
 
