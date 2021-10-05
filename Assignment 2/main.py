8a49ac68d4ea2b95857b469f503a416b6620bceb
TennantQuinton <quintonimo1998@gmail.com>
2021-10-05 10:23:36 -0400

    Update main.py
    Cleaning up code + getting training the model working

--- Assignment 2/main.py
+++ Assignment 2/main.py
@@ -5,20 +5,16 @@ Assignment 2
     ID: 20717788
 '''
 
+from operator import truediv
 import os, json, argparse, numpy as np, matplotlib.pyplot as plt, pandas as pd
 import torch as T, torch.nn as nn, torchvision as TV, torchvision.transforms as tf, torch.optim as optim
 from torchvision.utils import make_grid
 
 def conversion(input_data):
-    # Training data split (take the middle 20992 digits) in Pandas df format
-    training = (input_data[5500:-2999])
+    # Training data split (take the first 26492 digits) in Pandas df format
+    training = (input_data[0:-2999])
     training_data = training[training.columns[:-1]]
     training_labels = training[training.columns[-1]]
-
-    # Set 5500 of the digits as a validation set in Pandas df format
-    validation = (input_data[0:5501])
-    validation_data = validation[validation.columns[:-1]]
-    validation_labels = validation[validation.columns[-1]]
 
     # Testing data split (take the final 3000 digits) in Pandas df format
     testing = (input_data[26491:])
@@ -28,24 +24,18 @@ def conversion(input_data):
     # Convert to numpy array
     training_data = np.array(training_data)
     training_labels = np.array(training_labels)
-    validation_data = np.array(validation_data)
-    validation_labels = np.array(validation_labels)
     testing_data = np.array(testing_data)
     testing_labels = np.array(testing_labels)
 
     # Reshape to the correct dimensions for analysis
     training_data = training_data.reshape((-1, 1, 14, 14))
     training_labels = training_labels.reshape(training_labels.shape[0])
-    validation_data = validation_data.reshape((-1, 1, 14, 14))
-    validation_labels = validation_labels.reshape(validation_labels.shape[0])
     testing_data = testing_data.reshape((-1, 1, 14, 14))
     testing_labels = testing_labels.reshape(testing_labels.shape[0])
 
     # Convert to PyTorch tensor and normalize values
     training_data_tens = T.tensor(training_data)/255.0
     training_labels_tens = T.tensor(training_labels)
-    validation_data_tens = T.tensor(validation_data)/255.0
-    validation_labels_tens = T.tensor(validation_labels)
     testing_data_tens = T.tensor(testing_data)/255.0
     testing_labels_tens = T.tensor(testing_labels)
 
@@ -54,39 +44,63 @@ def conversion(input_data):
     # Convert to tensor with both data and labels. Then load the data
     training_tensor = T.utils.data.TensorDataset(training_data_tens, training_labels_tens)
     testing_tensor = T.utils.data.TensorDataset(testing_data_tens, testing_labels_tens)
-    validation_tensor = T.utils.data.TensorDataset(validation_data_tens, validation_labels_tens)
     load_training = T.utils.data.DataLoader(training_tensor, batch_size = batch_size, shuffle = True)
     load_testing = T.utils.data.DataLoader(testing_tensor, batch_size = batch_size, shuffle = True)
-    load_validation = T.utils.data.DataLoader(validation_tensor, batch_size = batch_size, shuffle = True)
 
     return (load_training, load_testing)
 
-def training(load_training, load_testing):
+def training_testing(load_training, load_testing):
 
     dataiter = iter(load_training)
     images, labels = dataiter.next()
 
+    # get inputs from json file
     input_size = 14*14 # =196
     hidden_layer = 100
     output_size = 9
     learning_rate = 0.001
+    epoch_max = 30
 
+    # Initialize the model
     model = nn.Sequential(nn.Linear(input_size, hidden_layer),
                             nn.ReLU(),
                             nn.Linear(hidden_layer, output_size),
                             nn.LogSoftmax(dim=1))
 
+    # Optimizer/loss functions used for learning
     optimizer = optim.Adam(model.parameters(), learning_rate)
-    criterion = nn.NLLLoss()
+    nn_loss = nn.NLLLoss()
+
+    # Set the data used for the images and labels within the learning
     images, labels = next(iter(load_training))
     images = images.view(images.shape[0], -1)
 
+    # Get the probabilities (log from model) and the loss initials
     log_probs = model(images)
-    loss = criterion(log_probs, labels)
+    loss = nn_loss(log_probs, labels)
 
-    epoch_max = 15
+    # Loop over the number of epochs specified
     for epoch in np.arange(0, epoch_max+1, 1):
-        print()
+        # Intialize the total loss to be updated over loops
+        loss_total = 0
+
+        # Loop over the images and labels we found earlier
+        for images, labels in load_training:
+            images = images.view(images.shape[0], -1)       # Re-flatten the images
+            optimizer.zero_grad()                           # Run the training optimizer with the set model
+            output = model(images)                          # Find the output
+            loss = nn_loss(output, labels)                  #   and the loss
+            loss.backward()                                 # backpropogate
+            optimizer.step()                                # Continue
+            loss_total += loss.item()                       # Update the total loss
+        
+        # Print an update
+        print('Epoch [{}/{}]'.format(epoch, num_epochs)+\
+            '\tTraining Loss: {:.4f}'.format(train_val)+\
+            '\tTest Loss: {:.4f}'.format(test_val))
+        print('Epoch: {0}, Training Loss: {1}'.format(epoch, loss_total/(len(load_training))))
+
+
 
 if __name__ == '__main__':
     # Command line arguments
@@ -104,5 +118,5 @@ if __name__ == '__main__':
 
     data_in = pd.read_csv("{0}/{1}".format(my_absolute_dirpath, in_file), delimiter=' ')
     converted_data = conversion(data_in)
-    training(converted_data[0], converted_data[1])
+    training_testing(converted_data[0], converted_data[1])
 
