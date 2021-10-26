5972e6e1428eacaf5ad1ca81f5af6c0088cedd89
TennantQuinton <quintonimo1998@gmail.com>
2021-10-26 12:52:54 -0400

    Update main.py
    Starting

--- Assignment 3/main.py
+++ Assignment 3/main.py
@@ -1,4 +1,13 @@
-# Write your assignment here
+'''
+PHYS 449 -- Fall 2021
+Assignment 3
+    Name: Quinton Tennant
+    ID: 20717788
+'''
+
+import os, json, argparse, numpy as np, matplotlib.pyplot as plt, pandas as pd
+import torch as T, torch.nn as nn, torch.optim as optim
+
 
 if __name__ == '__main__':
     print('hello')
