a28785572e92b30316d5dd0912b9a1457e774cd8
TennantQuinton <quintonimo1998@gmail.com>
2021-10-27 15:01:35 -0400

    Update main.py
    Started neural network

--- Assignment 3/main.py
+++ Assignment 3/main.py
@@ -14,27 +14,36 @@ def ode_sol(lb, ub, ntests):
         rand_x, rand_y = rd.randrange(lb*1000, ub*1000)/1000, rd.randrange(lb*1000, ub*1000)/1000
         print(rand_x, rand_y)
 
-        x, y = np.meshgrid(np.arange(lb, ub+(abs(lb-ub)/10), (abs(lb-ub)/10)), np.arange(lb, ub+(abs(lb-ub)/10), (abs(lb-ub)/10)))
+        x_vects, y_vects = np.meshgrid(np.arange(lb, ub+(abs(lb-ub)/10), (abs(lb-ub)/10)), np.arange(lb, ub+(abs(lb-ub)/10), (abs(lb-ub)/10)))
     
-        u = -y/np.sqrt(x**2 + y**2) #lambda x, y: -y/np.sqrt(x**2 + y**2)
-        v = x/np.sqrt(x**2 + y**2) #lambda x, y: x/np.sqrt(x**2 + y**2)
+        u = -y_vects/np.sqrt(x_vects**2 + y_vects**2) #lambda x, y: -y/np.sqrt(x**2 + y**2)
+        v = x_vects/np.sqrt(x_vects**2 + y_vects**2) #lambda x, y: x/np.sqrt(x**2 + y**2)
 
-        plt.quiver(x, y, u, v)
-        plt.scatter(rand_x, rand_y)
-        plt.show()
+        plt.scatter(rand_x, rand_y, color = 'red', zorder = 1)
+    plt.quiver(x_vects, y_vects, u, v, zorder = 0)
+    plt.show()
+
+    model = nn.Sequential(
+        nn.Linear(1, 50), 
+        nn.Sigmoid(), 
+        nn.Linear(50,1, bias=False)
+    )
+
+    optimizer = optim.Adam(model.parameters())
+    nn_loss = nn.CrossEntropyLoss()
 
 
 if __name__ == '__main__':
     # Command line arguments
     parser = argparse.ArgumentParser(description='Assignment 3: Tennant, Quinton (20717788)')
-    parser.add_argument('--param', default='param/param.json', help='relative file path for json attributes')
+    parser.add_argument('--param', default='param/param.json', help='relative file path for json attributes (default = \'param/param.json\')')
     parser.add_argument('-v', default=1, help='verbosity (default = 1)', type=float)
-    parser.add_argument('--res_path', default='plots/', help='relative path to save the test plots at')
-    parser.add_argument('--x_field', default='x**2', help='expression of the x-component of the vector field')
-    parser.add_argument('--y_field', default='y**2', help='expression of the y-component of the vector field')
-    parser.add_argument('--lb', default=-1, help='lower bound for initial conditions', type=int)
-    parser.add_argument('--ub', default=+1, help='upper bound for initial conditions', type=int)
-    parser.add_argument('--n_tests', default=3, help='number of test trajectories to plot', type=int)
+    parser.add_argument('--res_path', default='plots/', help='relative path to save the test plots at (default = \'plots/\')')
+    parser.add_argument('--x_field', default='x**2', help='expression of the x-component of the vector field (default = x**2)')
+    parser.add_argument('--y_field', default='y**2', help='expression of the y-component of the vector field (default = y**2)')
+    parser.add_argument('--lb', default=-1, help='lower bound for initial conditions (default = -1)', type=int)
+    parser.add_argument('--ub', default=+1, help='upper bound for initial conditions (default = +1)', type=int)
+    parser.add_argument('--n_tests', default=3, help='number of test trajectories to plot (default = 3)', type=int)
 
     # Receiving the command line arguments
     args = parser.parse_args()
