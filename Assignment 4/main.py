02cc0da8e8be2fe90c13156358a53588a453efd9
TennantQuinton <quintonimo1998@gmail.com>
2021-11-10 15:54:20 -0500

    Update main.py
    Initial work

--- Assignment 4/main.py
+++ Assignment 4/main.py
@@ -10,5 +10,33 @@ import os, json, argparse, numpy as np, matplotlib.pyplot as plt
 import torch as T, torch.nn as nn, torch.optim as optim
 from torch.nn.modules.loss import MSELoss
 
+def boltz_mach(data):
+    for line in data:
+        i_list = []
+        for i in line:
+            i_list.append(int(('{0}1'.format(i))))
+        print(i_list)
+
 if __name__ == '__main__':
-    print('hello')
+    # Command line arguments
+    parser = argparse.ArgumentParser(description='Assignment 3: Tennant, Quinton (20717788)')
+    parser.add_argument('-data_path', default='data/in.txt', help='relative file path for data input')
+
+    # Receiving the command line arguments
+    args = parser.parse_args()
+
+    # Turning the arguments into variables
+    data_path = args.data_path
+
+    # Getting the absolute file path from the relative
+    my_absolute_dirpath = os.path.abspath(os.path.dirname(__file__))
+    
+    # Checking if the in file exists
+    if os.path.isfile("{0}/{1}".format(my_absolute_dirpath, data_path)):
+        # Reading in the data and running the linear regression
+        data_in = np.loadtxt("{0}/{1}".format(my_absolute_dirpath, data_path), dtype=str)
+    else:
+        print('Filepath {0}/{1} does not exist'.format(my_absolute_dirpath, data_path))
+    
+    # Call the function
+    boltz_mach(data_in)
