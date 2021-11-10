802de00e3e9cd85ca15e0b0a2790563d227dc20a
TennantQuinton <quintonimo1998@gmail.com>
2021-11-10 14:54:32 -0500

    Starting Assignment

--- Assignment 4/main.py
+++ Assignment 4/main.py
@@ -1,4 +1,14 @@
-# Write your assignment here
+'''
+PHYS 449 -- Fall 2021
+Assignment 4
+    Name: Quinton Tennant
+    ID: 20717788
+'''
+
+# Imports
+import os, json, argparse, numpy as np, matplotlib.pyplot as plt
+import torch as T, torch.nn as nn, torch.optim as optim
+from torch.nn.modules.loss import MSELoss
 
 if __name__ == '__main__':
     print('hello')
